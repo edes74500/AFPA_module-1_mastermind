304c189819b9dd2161085ba48c8c662897daaec3
KLEIN Jean-Daniel <kleinjeandaniel@gmail.com>
2024-02-16 15:16:56 +0100

    m

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const solutionDisplayContainer = document.getElementById("solution");
 const form = document.getElementById("form");
 const input = form.querySelectorAll('input[type="number"]');
+let slots = document.querySelectorAll(".slot");
 
-let colors = ["red", "blue", "green", "yellow", "purple"];
+let colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "black", "white"];
 
-let maxColor = 4;
-let propositionSize = 4;
+let maxColor = 6;
+let maxProposition = 4;
 let solution = [];
 let proposition = [];
+let goodPlaceColor = [];
+let goodPlaceIndex = [];
+let goodColor = [];
+let tryCounter = 0;
 
-const randomNumber = () => Math.ceil(Math.random() * maxColor);
+//Create a random solution
+const randomNumber = () => Math.floor(Math.random() * maxColor);
 const randomSolution = () => {
-  for (let i = 0; i < maxColor; i++) {
+  for (let i = 0; i < maxProposition; i++) {
     let number;
     number = randomNumber();
     solution.push(number);
@@ -20,18 +26,89 @@ const randomSolution = () => {
 };
 randomSolution();
 
-let goodPlaceColor = [];
-let goodPlaceIndex = [];
-let goodColor = [];
-let tryCounter = 0;
+//Create a container for the all available colors
+const colorChoiceContainerDisplay = () => {
+  let colorChoicesContainer = document.querySelector(".color-choices-container");
+  for (let i = 0; i < maxColor; i++) {
+    let colorChoice = document.createElement("div");
+    colorChoice.classList.add("circle");
+    colorChoice.draggable = true;
+    colorChoice.dataset.color = i;
+    colorChoice.style.backgroundColor = colors[i];
+    colorChoicesContainer.appendChild(colorChoice);
+  }
+};
+colorChoiceContainerDisplay();
 
+//create a container for propositions slots
+const propositionSlotContainerDisplay = () => {
+  let propositionSlotContainer = document.querySelector(".user-color-choices-container");
+  for (let i = 0; i < maxProposition; i++) {
+    let propositionSlot = document.createElement("div");
+    propositionSlot.classList.add("slot");
+    // propositionSlot.dataset.color = i;
+    propositionSlotContainer.appendChild(propositionSlot);
+  }
+};
+propositionSlotContainerDisplay();
+
+//push current proposition to the porposition array
+const setCurrentProposition = () => {
+  proposition = [];
+  slots.forEach((slot) => {
+    proposition.push(parseInt(slot.dataset.color));
+  });
+  console.log("la proposition est " + proposition);
+};
+
+//Check if the proposition is full
+let allInputHasValue = true;
+const checkInputValue = (e) => {
+  console.log("check for value");
+  allInputHasValue = true;
+  slots = document.querySelectorAll(".slot");
+  slots.forEach((slot) => {
+    if (!slot.dataset.color) {
+      allInputHasValue = false;
+    }
+  });
+};
+
+//Add event listeners to drag colors to the slots
+document.querySelectorAll(".circle").forEach((circle) => {
+  circle.addEventListener("dragstart", (e) => {
+    e.dataTransfer.setData("color", e.target.style.backgroundColor);
+    e.dataTransfer.setData("index", e.target.dataset.color);
+  });
+});
+
+document.querySelectorAll(".slot").forEach((slot) => {
+  slot.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    // const color = e.dataTransfer.getData("color");
+    e.currentTarget.style.transform = "scale(1.1)";
+    // e.target.style.backgroundColor = color;
+  });
+  slot.addEventListener("dragleave", (e) => {
+    e.currentTarget.style.transform = "scale(1)";
+  });
+  slot.addEventListener("drop", (e) => {
+    const color = e.dataTransfer.getData("color");
+    const index = e.dataTransfer.getData("index");
+    // e.preventDefault();
+    e.target.style.backgroundColor = color;
+    e.target.dataset.color = index;
+  });
+});
+
+//Check if the proposition is correct
 const masterMind = () => {
   tryCounter++;
   goodPlaceColor = [];
   goodPlaceIndex = [];
   goodColor = [];
-  let a = solution;
-  let b = proposition;
+  let a = proposition;
+  let b = solution;
 
   const goodPlaceChecker = () => {
     for (let i = 0; i < a.length; i++) {
@@ -58,49 +135,35 @@ const masterMind = () => {
   goodPlaceChecker();
   goodColorChecker();
 };
-const setInputResult = () => {
-  proposition = [];
-  form.querySelectorAll('input[type="number"]').forEach((input) => {
-    // proposition = [];
-    proposition.push(parseInt(input.value));
-  });
-};
 
-input.forEach((input) => {
-  input.addEventListener("change", (e) => {
-    if (e.target.value > maxColor) {
-      e.target.value = 1;
-    }
-    if (e.target.value < 1) {
-      e.target.value = 4;
-    }
-    input.style.backgroundColor = colors[e.target.value - 1];
-    // input.style.color = colors[e.target.value - 1];
-  });
-});
+// input.forEach((input) => {
+//   input.addEventListener("change", (e) => {
+//     if (e.target.value > maxColor) {
+//       e.target.value = 1;
+//     }
+//     if (e.target.value < 1) {
+//       e.target.value = 4;
+//     }
+//     input.style.backgroundColor = colors[e.target.value - 1];
+//     // input.style.color = colors[e.target.value - 1];
+//   });
+// });
 
-let allInputHasValue = true;
-const checkInputValue = (e) => {
-  allInputHasValue = true;
-  input.forEach((input) => {
-    if (input.value === "") {
-      allInputHasValue = false;
-    }
-  });
-};
-
+//check if the game is over by time or if the proposition is correct
+let gameIsOver;
 const gameIsOverChecker = () => {
-  if (goodPlaceColor?.length === maxColor || tryCounter === 10) {
+  if (goodPlaceColor?.length === maxProposition || tryCounter === 10) {
     return true;
   } else {
     return false;
   }
 };
 
+//display winning or loosing message
 const showResult = () => {
   resultDisplay = document.getElementById("result");
   console.log(goodPlaceColor.length);
-  if (goodPlaceColor.length == maxColor) {
+  if (goodPlaceColor.length == maxProposition) {
     resultDisplay.innerHTML = `
   <h2> VICTORY!! En ${tryCounter} essais</h2>
   <iframe src="https://gifer.com/embed/6U6m" width=480 height=294.857 frameBorder="0" allowFullScreen></iframe><p>`;
@@ -108,7 +171,7 @@ const showResult = () => {
     resultDisplay.innerHTML = `<h2> GAME OVER!! </h2>
     <p> Solution was: ${solution
       .map((solution) => {
-        return `<span class="span-color" style="background-color: ${colors[solution - 1]}"></span>`;
+        return `<span class="span-color" style="background-color: ${colors[solution]}"></span>`;
       })
       .join("")} </p>
   <iframe src="https://gifer.com/embed/7q09" width=480 height=480.000 frameBorder="0" allowFullScreen></iframe><p>
@@ -116,9 +179,9 @@ const showResult = () => {
   }
 };
 
+//display current try result
 showCurrentTryResult = () => {
-  let wrongColorAndPlace = propositionSize - goodPlaceColor.length - goodColor.length;
-
+  let wrongColorAndPlace = maxProposition - goodPlaceColor.length - goodColor.length;
   let wrongColorAndPlaceSpans = "";
   for (let i = 0; i < wrongColorAndPlace; i++) {
     wrongColorAndPlaceSpans += `<span class="result-box_pin" style="background-color: rgba(255, 255, 255, 0.259)
@@ -130,7 +193,7 @@ showCurrentTryResult = () => {
   <p>  ${tryCounter}/10 </p>
   <p>${proposition
     .map((proposition) => {
-      return `<span class="span-color" style="background-color: ${colors[proposition - 1]}"></span>`;
+      return `<span class="span-color" style="background-color: ${colors[proposition]}"></span>`;
     })
     .join("")}</p>
        <div class="result-box">
@@ -151,9 +214,9 @@ showCurrentTryResult = () => {
 </div>
   </div>
   `;
+  scrollToBottom();
 };
 
-let gameIsOver;
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (gameIsOver) {
@@ -163,7 +226,7 @@ form.addEventListener("submit", (e) => {
   checkInputValue();
   if (!allInputHasValue) return;
   if (allInputHasValue) {
-    setInputResult();
+    setCurrentProposition();
     masterMind();
   }
 
@@ -174,3 +237,8 @@ form.addEventListener("submit", (e) => {
     showResult();
   }
 });
+
+function scrollToBottom() {
+  const container = document.getElementById("solution");
+  container.scrollTop = container.scrollHeight;
+}

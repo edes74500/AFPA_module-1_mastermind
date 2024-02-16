2433225529cb01134020fdd35cb081c6ade530db
KLEIN Jean-Daniel <kleinjeandaniel@gmail.com>
2024-02-16 11:43:07 +0100

    2

--- index.js
+++ index.js
@@ -56,18 +56,6 @@ const masterMind = () => {
   };
   goodPlaceChecker();
   goodColorChecker();
-  solutionDisplayContainer.innerHTML += `
-  <div class="try-container">
-  <h2> Essai ${tryCounter} </h2>
-  <p> Votre proposition est : ${proposition
-    .map((proposition) => {
-      return `<span class="span-color" style="background-color: ${colors[proposition - 1]}"></span>`;
-    })
-    .join("")}</p>
-  <p> Vous avez trouvé ${goodPlaceColor.length} couleurs bien place</p>
-  <p> Vous avez trouvé ${goodColor.length} couleurs bonne mais mal place</p>
-  </div>
-  `;
 };
 const setInputResult = () => {
   proposition = [];
@@ -79,6 +67,12 @@ const setInputResult = () => {
 
 input.forEach((input) => {
   input.addEventListener("change", (e) => {
+    if (e.target.value > maxColor) {
+      e.target.value = 1;
+    }
+    if (e.target.value < 1) {
+      e.target.value = 4;
+    }
     input.style.backgroundColor = colors[e.target.value - 1];
     // input.style.color = colors[e.target.value - 1];
   });
@@ -94,11 +88,65 @@ const checkInputValue = (e) => {
   });
 };
 
+const gameIsOverChecker = () => {
+  if (goodPlaceColor?.length === maxColor || tryCounter === 10) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const showResult = () => {
+  resultDisplay = document.getElementById("result");
+  console.log(goodPlaceColor.length);
+  if (goodPlaceColor.length == maxColor) {
+    resultDisplay.innerHTML = `
+  <h2> VICTORY!! En ${tryCounter} essais</h2>
+  <iframe src="https://gifer.com/embed/6U6m" width=480 height=294.857 frameBorder="0" allowFullScreen></iframe><p>`;
+  } else {
+    resultDisplay.innerHTML = `<h2> GAME OVER!! </h2>
+    <p> la solution est : ${solution
+      .map((solution) => {
+        return `<span class="span-color" style="background-color: ${colors[solution - 1]}"></span>`;
+      })
+      .join("")} </p>
+  <iframe src="https://gifer.com/embed/7q09" width=480 height=480.000 frameBorder="0" allowFullScreen></iframe><p>`;
+  }
+};
+
+showCurrentTryResult = () => {
+  solutionDisplayContainer.innerHTML += `
+  <div class="try-container">
+  <h2> Essai ${tryCounter} </h2>
+  <p> Votre proposition est : ${proposition
+    .map((proposition) => {
+      return `<span class="span-color" style="background-color: ${colors[proposition - 1]}"></span>`;
+    })
+    .join("")}</p>
+  <p> Vous avez trouvé ${goodPlaceColor.length} couleurs bien place</p>
+  <p> Vous avez trouvé ${goodColor.length} couleurs bonne mais mal place</p>
+  </div>
+  `;
+};
+
+let gameIsOver;
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  if (gameIsOver) {
+    return;
+  }
   checkInputValue();
+
   if (allInputHasValue) {
     setInputResult();
     masterMind();
+  }
+
+  gameIsOver = gameIsOverChecker();
+
+  if (gameIsOver) {
+    showResult();
+  } else {
+    showCurrentTryResult();
   }
 });
